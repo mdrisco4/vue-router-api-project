e16a365e14be051c7d9b7a6870ebc953151af892
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-01-25 16:41:26 -0500

    adding second api call on third page trying to fetch data I want to use.  need to fix/find an api that is responsive and properly allows my get.  also need to test a bit with responsive css for screen sizes.

--- src/views/AdditionalView.vue
+++ src/views/AdditionalView.vue
@@ -3,3 +3,39 @@
       <h1>This is page 3</h1>
     </div>
   </template>
+
+
+
+
+  <script>
+// import axios from "axios";
+// export default {
+//     data() {
+//         return {
+//             res: [],
+//         };
+//     },
+//     name: "App",
+//     methods: {
+//         // The get method called by the function
+//         onGet() {
+//             axios
+//                 .get("https://jsonplaceholder.typicode.com/todos")
+//                 .then((response) => {
+//                     console.log(response);
+//                     // using stringify to beautify the output
+//                     this.res = JSON.stringify(response.data);
+//                 })
+//                 .catch((errors) => {
+//                     console.log(errors); // Errors
+//                 });
+//         },
+//     },
+// };
+</script>
+
+
+
+<style>
+
+</style>
