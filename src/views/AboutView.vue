79cdf9f554cd38e31e7f34b77bd92d1a2bb2fe85
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-01-25 16:38:41 -0500

    added an api call and parsing test data

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul class="item" v-for="item in list" :key="item.id">
+      <li>{{ item.id }}</li>
+      <li>{{ item.email }}</li>
+      <li>{{ item.first_name }} {{ item.last_name }}</li>
+      <li>
+        <img :src="item.avatar" />
+      </li>
+    </ul>
   </div>
 </template>
+
+
+<script>
+import axios from "axios";
+export default {
+  name: "HomeView",
+  // components: {
+  //   // HelloWorld
+  // },
+  data() {
+    return {
+      list: [],
+    };
+  },
+
+  async mounted() {
+
+// let result = await axios.get("https://rest.coinapi.io/v1/quotes/latest");
+// let result = await axios.get("https://api.coinranking.com/v2");
+
+// let result = await axios.get("https://api.coinranking.com/v2/coins");
+
+let result = await axios.get("https://reqres.in/api/users?page=1");
+// console.log(result.data.data);
+this.list = result.data.data;
+},
+};
+</script>
+
+<style>
+.item {
+  display: flex;
+  justify-content: center;
+}
+
+.item li {
+  display: inline-block;
+  border: 1px solid black;
+  width: 180px;
+  text-align: center;
+  padding: 5px;
+}
+
+.item img {
+  width: 50px;
+}
+
+</style>
