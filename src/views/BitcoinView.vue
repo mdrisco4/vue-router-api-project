5a3445371b62366d2a7b9dd713fc06c608f426b5
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-02-06 23:02:06 -0500

    added basic classes and first bits of CSS, need to flesh out a good layout and styl for the coin pages

--- src/views/BitcoinView.vue
+++ src/views/BitcoinView.vue
@@ -1,41 +1,39 @@
 <template>
-    <div class="home">
-      <div class="container">
-        <div>BITCOIN</div>
-        <!-- <h1 class="intro">Welcome to Crypto Marketplace</h1> -->
-      </div>
+    <div class="container">
+    <div class="title">BITCOIN</div>
+    <div class="intro-information">Some information and a blurb about Bitcoin</div>
     </div>
-    <div class="data-label">Label</div>
+    <div class="data-label">24-Hour Change</div>
     <div class="data-value">{{ cryptos.CHANGE24HOUR }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">Change Today</div>
     <div class="data-value">{{ cryptos.CHANGEDAY }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">1-Hour Change</div>
     <div class="data-value">{{ cryptos.CHANGEHOUR }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">24-Hour % Change</div>
     <div class="data-value">{{ cryptos.CHANGEPCT24HOUR }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">Change % Today</div>
     <div class="data-value">{{ cryptos.CHANGEPCTDAY }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">1-Hour % Change</div>
     <div class="data-value">{{ cryptos.CHANGEPCTHOUR }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">Total Supply</div>
     <div class="data-value">{{ cryptos.CIRCULATINGSUPPLY }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">Market Cap</div>
     <div class="data-value">{{ cryptos.CIRCULATINGSUPPLYMKTCAP }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">Daily Opening</div>
     <div class="data-value">{{ cryptos.OPENDAY }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">Hourly Opening</div>
     <div class="data-value">{{ cryptos.OPENHOUR }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">24-Hour Volume</div>
     <div class="data-value">{{ cryptos.VOLUME24HOUR }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">--unknown--</div>
     <div class="data-value">{{ cryptos.VOLUME24HOURTO }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">Daily Volume</div>
     <div class="data-value">{{ cryptos.VOLUMEDAY }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">--unknown--</div>
     <div class="data-value">{{ cryptos.VOLUMEDAYTO }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">Hourly Volume</div>
     <div class="data-value">{{ cryptos.VOLUMEHOUR }}</div>
-    <div class="data-label">Label</div>
+    <div class="data-label">--unknown--</div>
     <div class="data-value">{{ cryptos.VOLUMEHOURTO }}</div>
   </template>
 
@@ -68,3 +66,22 @@ export default {
   },
 };
 </script>
+
+<style>
+.title {
+  font-size: 30px;
+}
+
+.intro-information {
+  font-size: 24px;
+}
+
+.data-label {
+  font-size: 20px;
+}
+
+.data-value {
+  font-size: 20px; 
+}
+
+</style>
